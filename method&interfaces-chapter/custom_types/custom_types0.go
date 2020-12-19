5973ef40b019b5a8a84ba97903da29c9ef1759c8
Gentjan Kolicaj <gentjankolicaj@yahoo.com>
2020-12-19 01:06:59 +0100

    New project for methods&interfaces chapter.

---
+++ method&interfaces-chapter/custom_types/custom_types0.go
@@ -0,0 +1,10 @@
+package main
+
+//Definition of some custom types
+//All these custom types can be used as receiver arguments on functions
+//Which would cause those functions to become methods of those types
+type MyBool bool
+type MyInt int
+type MyFloat float64
+type MyString string
+type MyComplex complex64
